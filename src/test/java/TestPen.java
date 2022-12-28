2d5869bf31445f12e643d1fabe005d10bb35843e
Eduard <Eduard.Harbunou@itechart-group.com>
2022-12-28 18:19:45 +0300

    Prepared code before refactoring

--- src/test/java/TestPen.java
+++ src/test/java/TestPen.java
@@ -4,10 +4,13 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Field;
 
 public class TestPen {
     SoftAssert softAssert = new SoftAssert();
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
     @Test (description = "Proverka pervogo konstruktora")
     public void checkConstructor1() throws NoSuchFieldException, IllegalAccessException {
         int testInkContainerValue = 6;
@@ -51,12 +54,10 @@ public class TestPen {
         Field privateField = Pen.class.getDeclaredField("inkContainerValue");
         privateField.setAccessible(true);
         int actualInkContainerValue = Integer.parseInt(privateField.get(testPen3).toString());
-        //int actualInkContainerValue = (int) privateField.get(testPen3);
 
         Field privateField2 = Pen.class.getDeclaredField("sizeLetter");
         privateField2.setAccessible(true);
         double actualSizeLetter = Double.parseDouble(privateField2.get(testPen3).toString());
-        //double actualSizeLetter = (double) privateField2.get(testPen3);
 
         Field privateField3 = Pen.class.getDeclaredField("color");
         privateField3.setAccessible(true);
@@ -87,19 +88,62 @@ public class TestPen {
     }
     @DataProvider
     public Object[][] colors() {
-        return new Object[][] {
+        return new Object[][]{
                 {"RED"},
                 {"BLUE"},
                 {"BLACK"}
         };
-
-
-
-
     }
 
+    @Test (dataProvider = "positiveWord")
+    public void testMetodaWritePositiveCase(int obemSterzhne, double shrift, String inputWord) {
+            Pen pen7 = new Pen(obemSterzhne, shrift);
+            Assert.assertEquals(pen7.write(inputWord), inputWord);
+    }
+    @DataProvider
+    public Object[][] positiveWord() {
+        return new Object[][]{
+                {5, 1.0, "Test"},
+                {45, 2.0, "Predlozhenie"},
+                {333, 4.0, "Supersochinenie"}
+        };
+    }
 
+    @Test (dataProvider = "negativeWordPystaiaStroka")
+    public void testMetodaWriteNegativeCase(int obemSterzhne, double shrift, String inputWord, String result) {
+        Pen pen7 = new Pen(obemSterzhne, shrift);
+        Assert.assertEquals(pen7.write(inputWord), result);
+    }
+    @DataProvider
+    public Object[][] negativeWordPystaiaStroka() {
+        return new Object[][]{
+                {-5, 1.0, "Test", ""},
+                {-0, 2.0, "Predlozhenie", ""},
+                {-333, 4.0, "Supersochinenie", ""}
+        };
+    }
 
+    @Test (dataProvider = "Proverka partOfWord")
+    public void testMetodaWriteNegativeCase2(int obemSterzhne, double shrift, String inputWord, String partOfWord) {
+        Pen pen8 = new Pen(obemSterzhne, shrift);
+        Assert.assertEquals(pen8.write(inputWord), partOfWord);
+    }
+    @DataProvider(name = "Proverka partOfWord")
+    public Object[][] partOfWord() {
+        return new Object[][]{
+                {3, 1.0, "Test", "Tes"},
+                {10, 2.0, "Predlozhenie", "Predl"},
+                {40, 4.0, "Supersochinenie", "Supersochi"}
+        };
+    }
 
+    @Test
+    public void testMetodaDoSomethingElse() {
+        String testColor = "Red";
+        System.setOut(new PrintStream(output));
+        Pen pen9 = new Pen(4,3.0, testColor);
+        pen9.doSomethingElse();
+        Assert.assertEquals(testColor,output.toString().trim());
+    }
 }
 
